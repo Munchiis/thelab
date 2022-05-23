fcd61ad6f2174c0c24017d2f22916d57eadebde5
somzsz <96028563+somzsz@users.noreply.github.com>
2022-05-22 19:42:18 -0700

    Still working on my path/map generation but here it is.

--- app.js
+++ app.js
@@ -4,6 +4,49 @@ import Player from "./player.js";
 
 const player = new Player
 const goal = new Character
+//Node for linked list for guaranteed path 
+class GuaranteedPathNode {
+    GuaranteedPathNode(typeOfTile, next, prev, x, y){
+        next = null;
+    }
+    //tyle type is 0 for floor and 1 for wall
+    typeOfTile;
+    x;
+    y;
+    next;
+    prev;
+}
+//Actual guaranteed path linked list as a class
+class GuaranteedPath{
+    head;
+    tail;
+    size;
+}
+/*My guaranteed path + map generation. HAS TO BE BIG ENOUGh
+    (I havent worked out the minimum size yet though). Also make sure
+    it is not a jagged array (i.e. every row has same number of
+    columns)*/
+/*I say make the arrays we try at least 10x10 but maybe 20x20 to be 
+    safe.*/
+function generateMap(arr){
+    let dimensionY = arr.length();
+    let dimensionX = arr[0].length();
+
+    var startY = Math.floor((dimensionY-1) / 2);
+    var startX = 0;
+
+    let goalY = startY;
+    let goalX = Math.floor((dimensionX-1) / 2);
+
+    arr[startY][startX] = 0;
+    arr[goalY][goalX] = 0;
+
+    let current = new GuaranteedPathNode();
+    while(GuaranteedPathNode.)
+
+
+}
+
 
 player.setCoor(1,4)
 goal.setCoor(2,2)
@@ -24,5 +67,6 @@ levels[0] = {
 }
 
 let myGame = new Game('game-container-1',levels[0])
+
 
 myGame.populateMap()
